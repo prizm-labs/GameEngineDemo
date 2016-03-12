9b35eec13899eb6529331d5d4d7c00969343aa72
John Dempsey <jaedempsey@gmail.com>
2016-03-11 17:01:38 -0800

    made prefab and some starting scripts

---
+++ Assets/Scripts/DraggableInstantiator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+public class DraggableInstantiator : MonoBehaviour {
+
+	//attach this script to a image that you can drag, when you release it, 
+	//it will destroy itself and instantiate a piece of the corresponding type
+
+	public TypeOfPiece typeToInstantiate;
+
+
+	void Start () {
+		gameObject.AddComponent<ApplyTransform> ();
+	}
+
+	void OnEnable() {
+		GetComponent<ReleaseGesture>().Released += HandleOnRelease;
+	}
+
+	void OnDisable() {
+		GetComponent<ReleaseGesture>().Released -= HandleOnRelease;
+	}
+
+	void HandleOnRelease (object sender, System.EventArgs e)
+	{
+		Debug.Log ("released, instantiating new object");
+
+		GameObject newPiece = Instantiate (Resources.Load ("Piece", typeof(GameObject))) as GameObject;
+		newPiece.GetComponent<Piece> ().myType = typeToInstantiate;
+		newPiece.GetComponent<Piece> ().Bootstrap ();
+	}
+
+}
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour {
+
+	//load mesh renderer
+	//if 2d object, load quad + image
+
+
+	private TypeOfPiece _myType;
+	public TypeOfPiece myType {
+		get { return _myType; }
+		set { _myType = value; }
+	}
+
+	private MeshRenderer meshRender;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent<ApplyTransform> ();
+	}
+
+	public void Bootstrap() {
+		StartCoroutine (_Bootstrap ());
+	}
+
+	private IEnumerator _Bootstrap() {
+		Debug.Log ("loading from streaming assets");
+
+		WWW data = new WWW (Application.streamingAssetsPath + "/" + myType.ToString ());
+		yield return data;
+
+		if (string.IsNullOrEmpty (data.error)) {
+			Debug.LogError ("error loading: " + myType.ToString ());
+		}
+
+		Debug.Log ("out of _Bootstrap()");
+
+	}
+
+}
