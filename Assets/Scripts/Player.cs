41a0ff97ab92e4b43dda9693e6bb147565ebfa28
John Dempsey <jaedempsey@gmail.com>
2016-03-11 18:38:00 -0800

    added spec.txt to root

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	private Color _myColor;
+	public Color myColor {
+		get { return _myColor;}
+		set {
+			_myColor = value;
+		}
+	}
+
+	public List<Piece> myOwnedPieces;
+
+	public int myNumSoldiers;
+	public int myNumCalvalry;
+	public int myNumCannons;
+
+	//should also update the player's displayer
+	void UpdateMyDockCard() {
+		myNumSoldiers = CountMyOwnedPieces (TypeOfPiece.soldier);
+		myNumCalvalry = CountMyOwnedPieces (TypeOfPiece.calvalry);
+		myNumCannons = CountMyOwnedPieces (TypeOfPiece.cannon);
+	}
+
+	//helper function to determine how many of a piece a player has
+	int CountMyOwnedPieces(TypeOfPiece typeToCount) {
+		int amnt = 0;
+
+		foreach (Piece peace in myOwnedPieces) {
+			if (peace.myType == typeToCount) {
+				amnt++;
+			}
+		}
+
+		return amnt;
+	}
+}
