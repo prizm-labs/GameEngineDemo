b32771128afeea1d0c8e49a6dd08c026240828ae
Danny <dny.ngyn@gmail.com>
2016-03-21 19:28:18 -0700

    merge

---
+++ Assets/_Scripts/RenderObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderObject : MonoBehaviour {
+
+	public Camera renderCamera;
+	public RenderTexture myRenderTexture;
+
+	public GameObject Model;
+
+	void Awake(){
+		getMyRenderCamera ();
+		SetNewRenderTexture ();
+
+
+	}
+
+	private void getMyRenderCamera(){
+		renderCamera = GetComponent<Camera>();
+	}
+
+
+	private void SetNewRenderTexture(){
+		myRenderTexture = new RenderTexture (256, 256, 16, RenderTextureFormat.ARGB32);
+		myRenderTexture.Create ();
+		renderCamera.targetTexture = myRenderTexture;
+	}
+}
