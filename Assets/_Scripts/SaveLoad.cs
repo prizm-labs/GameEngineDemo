2eb2515feaef45c9fb441cabbcee168b2c872fb2
Danny <dny.ngyn@gmail.com>
2016-03-23 14:30:59 -0700

    save menU

---
+++ Assets/_Scripts/SaveLoad.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class SaveLoad : MonoBehaviour {
+	[SerializeField]
+	private bool paused = false;
+	[SerializeField]
+	private GUITexture pausedGUI;
+	[SerializeField]
+	private string gameName = "Your Game";
+	[SerializeField]
+	private static bool logProgress = false;
+
+	private GameObject LoadButtonPrefab;
+	private GameObject SaveLoadContent;
+
+	void Awake(){
+		LoadButtonPrefab = Resources.Load ("GameStateButton/Pnl_GameState", typeof(GameObject)) as GameObject;
+		SaveLoadContent = GameObject.Find ("MainCanvas/Pnl_SaveLoad/ScrollView/ScrollRect/Content");
+	}
+
+
+	public void RefreshLoadGameButtons(){
+		foreach (Transform t in SaveLoadContent.transform) {
+			Destroy (t.gameObject);
+		}
+		for (int i = 0; i < LevelSerializer.SavedGames [LevelSerializer.PlayerName].Count; i++ ){
+			CreateNewButton (LevelSerializer.SavedGames [LevelSerializer.PlayerName] [i], LevelSerializer.SavedGames [LevelSerializer.PlayerName].Count - 1 - i);
+
+		}/*
+		foreach (LevelSerializer.SaveEntry sg in LevelSerializer.SavedGames[LevelSerializer.PlayerName]) {
+			CreateNewButton (sg);
+		}*/
+	}
+
+
+
+	private void CreateNewButton(LevelSerializer.SaveEntry sg, int index){
+		GameObject newButton = Instantiate (LoadButtonPrefab) as GameObject;
+		SaveLoadButton newButtonScript = newButton.GetComponent<SaveLoadButton> ();
+		newButton.SetParent (SaveLoadContent);
+		newButtonScript.SetSavedGameCaption (sg.Caption);
+		newButtonScript.setSavedGameNumber (index);
+		newButtonScript.LoadGameButton.onClick.AddListener(() => LevelSerializer.LoadNow(sg.Data));
+	}
+
+
+	public void SaveCurrentGame(){
+		LevelSerializer.SaveGame(gameName);
+		RefreshLoadGameButtons ();
+	}
+
+
+}
---
+++ Assets/_Scripts/SaveLoadButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SaveLoadButton : MonoBehaviour {
+	public Text SavedGameNumber;
+
+	public Text SavedGameCaption;
+	public Button LoadGameButton;
+
+	// Use this for initialization
+	void Awake () {
+		FindMyStuff ();
+	}
+
+	private void FindMyStuff(){
+		var allChildren = gameObject.GetComponentsInChildren<Transform> (true);
+		foreach (Transform t in allChildren) {
+			if (t.name == "Number")
+				SavedGameNumber = t.GetComponent<Text>();
+			if (t.name == "Caption")
+				SavedGameCaption = t.GetComponent<Text>();
+			if (t.name == "Btn_Gamestate")
+				LoadGameButton = t.GetComponent<Button> ();
+		}
+	}
+
+	public void setSavedGameNumber(int index){
+		SavedGameNumber.text = index + "";
+	}
+
+	public void SetSavedGameCaption(string caption){
+		SavedGameCaption.text = caption;
+	}
+}
