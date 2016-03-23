f19849e99af628c71ef1a37448e9080fdd83030d
John Dempsey <jaedempsey@gmail.com>
2016-03-23 09:30:37 -0700

    edited DragMe

--- Assets/Scripts/CategoryPopulator.cs
+++ Assets/Scripts/CategoryPopulator.cs
@@ -6,10 +6,11 @@ public class CategoryPopulator : MonoBehaviour {
 
 	public ObjectCreatorButtons typeToPopulate;
 	public GameObject picturePrefab;
+	Transform contentTransform;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		contentTransform = transform.Find ("Content");
 	}
 	
 	// Update is called once per frame
@@ -22,6 +23,8 @@ public class CategoryPopulator : MonoBehaviour {
 		}
 	}
 
+	//add Dragme, adds buttonName, type to instantiate
+
 	void MakePicture() {
 		GameObject rawImage = Instantiate (picturePrefab);
 		rawImage.transform.SetParent (this.transform);
@@ -31,5 +34,17 @@ public class CategoryPopulator : MonoBehaviour {
 		Debug.Log ("done with making the picture");
 
 		this.transform.localEulerAngles = new Vector3 (90, 0, 0);
+	}
+
+	public void MakeAllChildren() {
+		for (int i = 0; i < CategoryInitializer.Instance.allCachedTexturesList[typeToPopulate.ToString()].Count; i++) {
+			Debug.Log ("populating: " + CategoryInitializer.Instance.allCachedTexturesList [typeToPopulate.ToString ()] [i].name);
+			GameObject theImage = Instantiate (picturePrefab);
+			theImage.transform.SetParent (contentTransform);
+			theImage.GetComponent<RawImage> ().texture = CategoryInitializer.Instance.allCachedTexturesList [typeToPopulate.ToString ()] [i];
+
+			theImage.GetComponent<DragMe> ().buttonName = typeToPopulate;
+			theImage.GetComponent<DragMe> ().typeToInstantiate = TypeOfPiece.attackBot;	//change this
+		}
 	}
 }
